9d63f971d87a67bb600301b66ee1ac47331e815d
ameym <ameymojar@gmail.com>
2026-07-06 14:34:39 +0530

    feat: add simple calculator for basic arithmetic operations

---
+++ simple calculator.py
@@ -0,0 +1,7 @@
+num_1 = 356
+num_2 = 2565
+
+print("Addition of two numbers:", num_1 + num_2)
+print("Subtarction of two numbers:", num_1 - num_2)
+print("mulitplication of two numbers:", num_1 * num_2)
+print("Division of two numbers:", num_1 / num_2)
