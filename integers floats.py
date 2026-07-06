eccc667e473ba2bb678f588bb1b927b13cadd48a
ameym <ameymojar@gmail.com>
2026-07-06 14:06:06 +0530

    integers

---
+++ integers floats.py
@@ -0,0 +1,27 @@
+num_1 = 1
+num_2 = 2.5
+num_3 = -3.5
+num_4 ='100000000'
+num_5 = '250225'
+print(num_1 + num_2)
+print(num_1 * num_2)
+print(num_1 / num_2)
+print(num_1 - num_2)
+print(num_1 ** num_2)
+print(num_1 // num_2)
+print(num_1 % num_2)
+
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+
+print(round(num_2))
+print(abs(num_3))
+
+num_4 = int(num_4)
+num_5 = int(num_5)
+print(str(num_4) + str(num_5))
+print(num_4 + num_5)
