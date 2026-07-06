2af3ef2ac55a43737584ac96a1eb21aa8d612289
ameym <ameymojar@gmail.com>
2026-07-06 14:50:57 +0530

    feat: add script to find the largest and smallest of three numbers

---
+++ largest of threee.py
@@ -0,0 +1,6 @@
+num_1 = 3535535
+num_2 = 3535238
+num_3 = 4598573
+
+print("The largest number among the three numbers is:", max(num_1, num_2, num_3))
+print("The smallest number among the three numbers is:", min(num_1, num_2, num_3))
