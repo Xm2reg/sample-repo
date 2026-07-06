897e72c915cd162f82895328534ad64bf6696abc
ameym <ameymojar@gmail.com>
2026-07-06 14:40:47 +0530

    feat: add script to convert Celsius to Fahrenheit and vice versa

---
+++ Celsius to Fahrenheit.py
@@ -0,0 +1,9 @@
+num = 29
+
+fahrenheit = (num * 9/5) + 32
+
+celsius = (fahrenheit - 32) * 5/9
+
+print("Temperature provided was: ", num)
+print("Temperature in Fahrenheit:", fahrenheit)
+print("Temperature in Celsius:", celsius)   
