2222a7be3badf5ba28d067bfba2699f5c6ee5904
ameym <ameymojar@gmail.com>
2026-07-06 14:47:32 +0530

    feat: add script to check if numbers are even or odd

---
+++ even and odd.py
@@ -0,0 +1,6 @@
+num_1 = 10111
+nume_2 = 12325140
+
+print("true = even, false = odd")
+print(" Checking if the num_1 is even or odd:",num_1 % 2 == 0)
+print(" Checking if the num_2 is even or odd:",nume_2 % 2 == 0)
